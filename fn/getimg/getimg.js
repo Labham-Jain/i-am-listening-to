790550b17daa00777dd3e6780522c1a89574c62f
shivamjoker <singhshivamkr@gmail.com>
2021-02-13 14:14:22 +0530

    Added temp dir serving

---
+++ fn/getimg/getimg.js
@@ -0,0 +1,17 @@
+const fs = require("fs").promises;
+
+const handler = async (event) => {
+  const svg = await fs.readFile("/tmp/badge.svg");
+  try {
+    return {
+      statusCode: 200,
+      body: svg.toString(),
+      headers: { "content-type": "image/svg+xml" },
+      // isBase64Encoded: true,
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
+
+module.exports = { handler };
