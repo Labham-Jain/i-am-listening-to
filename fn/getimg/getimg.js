398b1eb23b28044a78cd33dd9b7a25748924404c
shivamjoker <singhshivamkr@gmail.com>
2021-02-18 16:03:09 +0530

    Fixed getimg route

--- fn/getimg/getimg.js
+++ fn/getimg/getimg.js
@@ -1,13 +1,27 @@
-const fs = require("fs").promises;
+const { Octokit } = require("@octokit/core");
+
+const gtoken = process.env.G_TOKEN;
+// github personal access token - https://github.com/settings/tokens/new?scopes=repo
+
+const octokit = new Octokit({ auth: gtoken });
 
 const handler = async (event) => {
-  const svg = await fs.readFile(`/tmp/badge.svg`);
   try {
+    // extract SVG info from the image
+    const fileInfo = await octokit.request(
+      "GET /repos/{owner}/{repo}/contents/{path}",
+      {
+        owner: "shivamjoker",
+        repo: "shivamjoker",
+        path: "music-badge.svg",
+      }
+    );
+
     return {
       statusCode: 200,
-      body: svg.toString(),
+      body: fileInfo.data.content,
       headers: { "content-type": "image/svg+xml" },
-      // isBase64Encoded: true,
+      isBase64Encoded: true,
     };
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
